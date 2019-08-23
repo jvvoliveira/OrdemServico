038f311261de175dd0a7365692a97840b4f2e2dd
Joao Victor Oliveira <jvv99.oliveira@gmail.com>
2019-08-23 17:16:30 -0300

    add todas entidades

---
+++ src/main/java/entidades/Equipamento.java
@@ -0,0 +1,121 @@
+package entidades;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_EQUIPAMENTO")
+public class Equipamento implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Column(name = "TB_DESCRICAO", length = 150, nullable = false)
+    private String descricao;
+    
+    @Column(name = "TB_MARCA", length = 30)
+    private String marca;
+    
+    @Column(name = "TB_MODELO", length = 30, nullable = false)
+    private String modelo;
+    
+    @Column(name = "TB_DEFEITO", length = 150, nullable = false)
+    private String defeito;
+    
+    @Column(name = "TB_SOLUCAO", length = 150)
+    private String solucao;
+    
+    @Column(name = "TB_CUSTO")
+    private double custo;
+    
+//    private Servico servico;
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + (int) (this.id ^ (this.id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Equipamento other = (Equipamento) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getDefeito() {
+        return defeito;
+    }
+
+    public void setDefeito(String defeito) {
+        this.defeito = defeito;
+    }
+
+    public String getSolucao() {
+        return solucao;
+    }
+
+    public void setSolucao(String solucao) {
+        this.solucao = solucao;
+    }
+
+    public double getCusto() {
+        return custo;
+    }
+
+    public void setCusto(double custo) {
+        this.custo = custo;
+    }
+    
+    
+    
+}
---
+++ src/main/java/entidades/Servico.java
@@ -0,0 +1,122 @@
+package entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import utils.Status;
+
+@Entity
+@Table(name = "TB_SERVICO")
+public class Servico implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(name = "COL_STATUS")
+    private Status status;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "COL_DATA_INICIO")
+    private Date inicio;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "COL_DATA_FIM")
+    private Date fim;
+    
+    @Transient //não armazenado, calculado
+    @Column(name = "COL_CUSTO_PECAS")
+    private double custoPecas;
+    
+    @Column(name = "COL_MAO_OBRA")
+    private double MaoDeObra;
+    
+//    private Cliente cliente
+//    private List<Funcionario> funcionarios;
+//    private List<Equipamento> equipamentos;
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 83 * hash + (int) (this.id ^ (this.id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Servico other = (Servico) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Date getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Date inicio) {
+        this.inicio = inicio;
+    }
+
+    public Date getFim() {
+        return fim;
+    }
+
+    public void setFim(Date fim) {
+        this.fim = fim;
+    }
+
+    public double getCustoPecas() {
+        return custoPecas;
+    }
+
+    public void setCustoPecas(double custoPecas) {
+        this.custoPecas = custoPecas;
+    }
+
+    public double getMaoDeObra() {
+        return MaoDeObra;
+    }
+
+    public void setMaoDeObra(double MaoDeObra) {
+        this.MaoDeObra = MaoDeObra;
+    }
+    
+    
+}
