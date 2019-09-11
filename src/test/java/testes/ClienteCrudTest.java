624d9269b6dc0180a47da8f38f78c671d7e1154e
BeaCristina <biacristinafs@hotmail.com>
2019-09-11 07:59:47 -0300

    Salvando crud teste funcionandogit pushgit pushgit pushgit push

--- src/test/java/testes/ClienteCrudTest.java
+++ src/test/java/testes/ClienteCrudTest.java
@@ -2,6 +2,9 @@ package testes;
 
 import entidades.Cliente;
 import entidades.Endereco;
+import entidades.Equipamento;
+import entidades.Funcionario;
+import entidades.Servico;
 import entidades.Telefone;
 import java.util.HashMap;
 import java.util.List;
@@ -94,14 +97,13 @@ public class ClienteCrudTest extends GenericTest{
         Cliente cliente = em.find(Cliente.class, 8L);
         assertNotNull(cliente);
         
-        System.out.println(cliente.getEndereco()==null);
         /*
         //retirar fk dos telefones desse cliente
-        String jpql = "SELECT t FROM Telefone t WHERE t.cliente.id = ?9";
+        String jpql = "SELECT t FROM Telefone t WHERE t.cliente.id = ?8";
         TypedQuery<Telefone> query = em.createQuery(jpql, Telefone.class);
         //obrigatoriamente ir para o banco
         query.setHint("javax.persistence.cache.retrieveMode", CacheRetrieveMode.BYPASS); 
-        query.setParameter(1, 9L);
+        query.setParameter(1, 8L);
         List<Telefone> telefone = query.getResultList();
         telefone.get(0).setCliente(null);
         em.flush();
@@ -112,7 +114,7 @@ public class ClienteCrudTest extends GenericTest{
         Cliente checkCliente = em.find(Cliente.class, 9L);
         assertNull(checkCliente);*/
         
-        
+        //removendo telefones - eles são apagados pois a coluna de fk não pode ter valores nulos
         String jpql = "SELECT t FROM Telefone t";
         TypedQuery<Telefone> query = em.createQuery(jpql, Telefone.class);
         //obrigatoriamente ir para o banco
@@ -122,24 +124,26 @@ public class ClienteCrudTest extends GenericTest{
         for(Telefone tel : telefones){
             if(tel.getCliente().getId() == cliente.getId()){
                System.out.println("REMOVENDO: "+tel.getCliente().getNome()+" - "+tel.getCliente().getId()+" E TELEFONE: "+tel.getNumero());
-               //Não posso ret 
                em.remove(tel);
             }
         }
-        //telefone.setCliente(null);
-        //em.flush();
         
-        //Endereco endereco = em.find(Endereco.class, cliente.getEndereco().getId()L);
-        //assertNotNull(endereco);
-        //em.remove(endereco);
-        //System.out.println("TESTEEEEEEEEEEE"+cliente.getEndereco().getId());
+        //remover de cliente de servico - o CLiente id(8) tem servico de id(5)
+        Servico servico = em.find(Servico.class, 5L);
+        assertNotNull(servico);
+        Funcionario funcionario = em.find(Funcionario.class, servico.getFuncionario().getId());
+        Equipamento equipamento = em.find(Equipamento.class, servico.getEquipamentos().get(0).getId());
+        cliente.setServicos(null);
+        funcionario.setServicos(null);
+        equipamento.setServico(null);
+        //Apagar servico porque fk_cli não pode fica nula
+        em.remove(servico);
+        
         em.remove(cliente);
         
         ((JpaCache)emf.getCache()).clear();
         Cliente checkCliente = em.find(Cliente.class, 8L);
         assertNull(checkCliente);
-        //Cliente checkCliente = em.find(Cliente.class, 5L);
-        //assertNotNull(telefone);
     }
     
 }
