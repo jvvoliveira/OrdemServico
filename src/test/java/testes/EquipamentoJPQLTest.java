5ce4ff06abe7e1767a0f73143d464a0dcb7d93f8
Joao Victor Oliveira <jvv99.oliveira@gmail.com>
2019-09-26 20:00:04 -0300

    testJQPL equipamento

--- src/test/java/testes/EquipamentoJPQLTest.java
+++ src/test/java/testes/EquipamentoJPQLTest.java
@@ -1,4 +1,3 @@
-
 package testes;
 
 import entidades.Equipamento;
@@ -7,47 +6,77 @@ import javax.persistence.CacheRetrieveMode;
 import javax.persistence.TypedQuery;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import utils.Status;
 
+public class EquipamentoJPQLTest extends GenericTest {
 
-public class EquipamentoJPQLTest extends GenericTest{
-    
     @Test
-    public void equipamentosDeUmCliente(){
+    public void equipamentosDeUmCliente() {
         String jpql = "SELECT e FROM Equipamento e WHERE e.servico.cliente.id = ?1";
         TypedQuery<Equipamento> query = em.createQuery(jpql, Equipamento.class);
         //obrigatoriamente ir para o banco
-        query.setHint("javax.persistence.cache.retrieveMode", CacheRetrieveMode.BYPASS); 
+        query.setHint("javax.persistence.cache.retrieveMode", CacheRetrieveMode.BYPASS);
         query.setParameter(1, 5L);
         List<Equipamento> equipamentos = query.getResultList();
         assertEquals(1, equipamentos.get(0).getId());
         assertEquals(1, equipamentos.size());
         assertEquals("Motorola", equipamentos.get(0).getMarca());
     }
-    
+
     @Test
-    public void equipamentosDeUmFuncionarioAtendente(){
+    public void equipamentosDeUmFuncionarioAtendente() {
         String jpql = "SELECT e FROM Equipamento e WHERE e.servico.funcionario.id = ?1";
         TypedQuery<Equipamento> query = em.createQuery(jpql, Equipamento.class);
         //obrigatoriamente ir para o banco
-        query.setHint("javax.persistence.cache.retrieveMode", CacheRetrieveMode.BYPASS); 
+        query.setHint("javax.persistence.cache.retrieveMode", CacheRetrieveMode.BYPASS);
         query.setParameter(1, 1L);
         List<Equipamento> equipamentos = query.getResultList();
         assertEquals(2, equipamentos.get(0).getId());
         assertEquals(4, equipamentos.size());
     }
-    
+
     @Test
-    public void equipamentosDeUmFuncionarioAtendenteFinalizados(){
-        String jpql = "SELECT s.equipamentos FROM Servico s WHERE s.status LIKE ?1 "
-                + "IN (SELECT s FROM Servico s WHERE s.funcionario.id = ?2)";
-//        SELECT i FROM Item i WHERE i.vendedor IN (SELECT v FROM Vendedor v WHERE v.reputacao = :reputacao)
+    public void equipamentosDeUmFuncionarioAtendenteFinalizados() {
+        String jpql = "SELECT e FROM Equipamento e WHERE e.servico "
+                + "IN (SELECT s FROM Servico s WHERE s.funcionario.id = ?1 AND s.status = ?2)";
+
         TypedQuery<Equipamento> query = em.createQuery(jpql, Equipamento.class);
+
         //obrigatoriamente ir para o banco
-        query.setHint("javax.persistence.cache.retrieveMode", CacheRetrieveMode.BYPASS); 
-        query.setParameter(1, "FINALIZADO");
-        query.setParameter(2, 2);
+        query.setHint("javax.persistence.cache.retrieveMode", CacheRetrieveMode.BYPASS);
+        query.setParameter(1, 2);
+        query.setParameter(2, Status.FINALIZADO);
         List<Equipamento> equipamentos = query.getResultList();
         assertEquals("MotoG G4", equipamentos.get(0).getModelo());
         assertEquals(1, equipamentos.size());
+    }
+
+    @Test
+    public void equipamentoComMaiorMaoDeObra() {
+        String jpql = "SELECT MAX(e.maoObra) FROM Equipamento e";
+
+        TypedQuery<Double> query = em.createQuery(jpql, Double.class);
+
+        //obrigatoriamente ir para o banco
+        query.setHint("javax.persistence.cache.retrieveMode", CacheRetrieveMode.BYPASS);
+        double maiorMaoDeObra = query.getSingleResult();
+
+        assertEquals(95.0, maiorMaoDeObra, 0);
+        //valor esperado = 95, intervalo entre valores deve ser 0
+    }
+
+    @Test
+    public void quantidadeDeEquipamentosQueTiveramServicoCancelado() {
+        String jpql = "SELECT COUNT(e) FROM Equipamento e where e.servico "
+                + "IN (SELECT s FROM Servico s WHERE s.status = ?1)";
+
+        TypedQuery<Long> query = em.createQuery(jpql, Long.class);
+        query.setParameter(1, Status.CANCELADO);
+        //obrigatoriamente ir para o banco
+        query.setHint("javax.persistence.cache.retrieveMode", CacheRetrieveMode.BYPASS);
+        Long qtdEquipamentosCancelados = query.getSingleResult();
+        
+        assertEquals(1L, qtdEquipamentosCancelados, 0);
+        //quantidade esperada = 1, intervalo entre valores deve ser 0
     }
 }
